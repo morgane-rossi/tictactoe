2a5182cf1b87eaa1e6cab7183022b536c69edf2d
jeremy-gavend <jeremy.gavend@laplateforme.io>
2024-10-23 14:29:12 +0200

    Fini l'affichage de la grille et la vérification

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def verifVainqueur(grille):
 
 #Le modulos préviennent de creer des traits verticaux supplémentaires après les lignes (il n'en crée pas aprñs index 2, 5 etc)
 #PAreil pour le dernier if qui évite un trait horizontal en trop
-#La fonction print directement, il suffit de l'appeler pour afficher affichageGrille(grille)
+#La fonction print directement, il suffit de l'appeler pour afficher la grille: affichageGrille(grille)
 
 def affichageGrille(grille):
     for x in range(len(grille)):
