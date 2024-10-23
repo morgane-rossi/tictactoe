b04744ca8f9c7c2499b01f165a0ab090f2696266
moussa-camara <moussa.camara@laplateforme.io>
2024-10-23 16:18:38 +0200

    les joueurs

--- main.py
+++ main.py
@@ -9,6 +9,24 @@
 
 
 #[Moussa] Ajouter/saisir des joueurs
+def demarrer_partie():
+    nombre_joueurs = int(input("Combien de joueurs ? "))
+    joueurs = []
+    for i in range(nombre_joueurs):
+        nom = input(f"Entrez le nom du joueur {i+1} : ")
+        joueurs.append(nom)
+
+    
+
+    joueur_actuel=0  # Index du joueur en cours
+    tour+=1
+    print(f"C'est au tour de {joueurs[joueur_actuel]} de jouer.")
+    joueur_actuel = (joueur_actuel + 1) % nombre_joueurs  # Passer au joueur suivant
+    return joueur_actuel
+demarrer_partie()
+
+
+
 
 
 
