5b6d7ea0a86231622452c07d97f7ffcbf0fffec6
jeremy-gavend <jeremy.gavend@laplateforme.io>
2024-10-23 10:26:30 +0200

    Commentaires pour savoir qui fait quoi

---
+++ main.py
@@ -0,0 +1,24 @@
+#[Morgane] Intro du jeu
+
+
+
+
+#[Morgane] Grille
+
+
+
+
+#[Moussa] Ajouter/saisir des joueurs
+
+
+
+
+#[Jeremy] Vérification de fin de partie
+
+
+
+
+#[Jeremy] Afficher la grille
+
+
+
