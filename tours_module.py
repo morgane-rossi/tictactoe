0507578d8507e7999f15ed9f2eef06afc13a53c9
morgane-du-tweize <morgane_rt@hotmail.com>
2024-10-25 12:40:05 +0200

    fix

--- tours_module.py
+++ tours_module.py
@@ -1,5 +1,8 @@
-def tour_suivant(joueurs, joueur_actuel):
-    joueur_actuel = (joueur_actuel + 1) % len(joueurs)  # Passer au joueur suivant
+<<<<<<< HEAD:tours_module.py
+def tourSuivant(joueurs, joueur_actuel, tour):
+    tour += 1
+    # Index du joueur en cours
+    print(f"len(joueurs) = {len(joueurs)}")
 
     print(f"\n\nC'est au tour de {joueurs[joueur_actuel][0]} de jouer.")
     return joueur_actuel
