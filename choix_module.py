553bfbe068fb639212935171d75a618666e9ee43
jeremy-gavend <jeremy.gavend@laplateforme.io>
2024-10-25 13:55:14 +0200

    Trying windows

--- choix_module.py
+++ choix_module.py
@@ -1,18 +1,11 @@
-
-import affichage_module
-import tour_module
-
-
-def choixCase(symbole, grille, tour):
-    
-import ia_module
+import ia_module as ia_mod
 
 def choixCase(grille, joueurs, joueur_actuel):
     #Boucle pour demander l'input tant que le joueur ne choisi pas une case valide
     choix_valide = False
     while choix_valide == False:
         if joueurs[1][0] == "Bot" and joueur_actuel == 1:
-            return ia_module.ia(grille, joueurs[1][1])
+            return ia_mod.ia(grille, joueurs[1][1])
         else:
             choix = int(input('Choissisez une case: '))
         #Envoie une erreur si la case est invalide (non-occupé)
