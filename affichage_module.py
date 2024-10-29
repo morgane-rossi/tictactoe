4cb512b20cd8950e4c0c31f4e97fce3532ae8281
jeremy-gavend <jeremy.gavend@laplateforme.io>
2024-10-29 09:35:04 +0100

    better graphics ne marche pas

--- affichage_module.py
+++ affichage_module.py
@@ -1,63 +1,47 @@
 import tkinter as tk
 from tkinter import ttk
-import main
+#import main
 
-def affichage_grille_tk(grille, joueurs, joueur_actuel):
-    fenetre = tk.Tk()
-    fenetre.title("Morpion")
-    fenetre.geometry("600x600")
+class affichage_grille_tk():
+    def __init__(self, grille, joueurs, joueur_actuel):
+        self.fenetre = tk.Tk()
+        self.fenetre.title("Morpion")
+        self.fenetre.geometry("600x600")
 
-    fenetre.columnconfigure(0, weight = 1)
-    fenetre.columnconfigure(1, weight = 1)
-    fenetre.columnconfigure(2, weight = 1)
+        self.fenetre.columnconfigure(0, weight = 1)
+        self.fenetre.columnconfigure(1, weight = 1)
+        self.fenetre.columnconfigure(2, weight = 1)
 
-    def button_u_l_clicked():
-        grille[0][0] = joueurs[joueur_actuel]
-        bouton_u_l.config(text = grille[0][0])
-        bouton_u_l.config(text = joueurs[joueur_actuel][1])
-    def button_u_c_clicked():
-        bouton_u_c.config(text = joueurs[joueur_actuel][1])
-    def button_u_r_clicked():
-        bouton_u_r.config(text = joueurs[joueur_actuel][1])
-    
-    def button_c_l_clicked():
-        bouton_c_l.config(text = joueurs[joueur_actuel][1])
-    def button_c_c_clicked():
-        bouton_c_c.config(text = joueurs[joueur_actuel][1])
-    def button_c_r_clicked():
-        bouton_c_r.config(text = joueurs[joueur_actuel][1])
+    def button_clicked(self):
+        print(self)
 
-    def button_b_l_clicked():
-        bouton_b_l.config(text = joueurs[joueur_actuel][1])
-    def button_b_c_clicked():
-        bouton_b_c.config(text = joueurs[joueur_actuel][1])
-    def button_b_r_clicked():
-        bouton_b_r.config(text = joueurs[joueur_actuel][1])
-        
+            #grille[0][0] = joueurs[joueur_actuel]
+            #bouton_u_l.config(text = grille[0][0])
+            #bouton_u_l.config(text = joueurs[joueur_actuel][1])            
 
-    bouton_u_l = ttk.Button(fenetre, text="1", command=button_u_l_clicked)
-    bouton_u_l.place(x=0, y=0, width=100, height=100)
-    bouton_u_c = ttk.Button(fenetre, text="2", command=button_u_c_clicked)
-    bouton_u_c.place(x=0, y=100, width=100, height=100)
-    bouton_u_r = ttk.Button(fenetre, text="3", command=button_u_r_clicked)
-    bouton_u_r.place(x=0, y=200, width=100, height=100)
+        bouton_u_l = ttk.Button(self.fenetre, text="1", command=self.button_clicked)
+        bouton_u_l.place(x=0, y=0, width=100, height=100)
+        bouton_u_c = ttk.Button(self.fenetre, text="2", command=self.button_u_c_clicked)
+        bouton_u_c.place(x=0, y=100, width=100, height=100)
+        bouton_u_r = ttk.Button(self.fenetre, text="3", command=self.button_u_r_clicked)
+        bouton_u_r.place(x=0, y=200, width=100, height=100)
 
-    bouton_c_l = ttk.Button(fenetre, text="4", command=button_c_l_clicked)
-    bouton_c_l.place(x=100, y=0, width=100, height=100)
-    bouton_c_c = ttk.Button(fenetre, text="5", command=button_c_c_clicked)
-    bouton_c_c.place(x=100, y=100, width=100, height=100)
-    bouton_c_r = ttk.Button(fenetre, text="6", command=button_c_r_clicked)
-    bouton_c_r.place(x=100, y=200, width=100, height=100)
+        bouton_c_l = ttk.Button(self.fenetre, text="4", command=self.button_c_l_clicked)
+        bouton_c_l.place(x=100, y=0, width=100, height=100)
+        bouton_c_c = ttk.Button(self.fenetre, text="5", command=self.button_c_c_clicked)
+        bouton_c_c.place(x=100, y=100, width=100, height=100)
+        bouton_c_r = ttk.Button(self.fenetre, text="6", command=self.button_c_r_clicked)
+        bouton_c_r.place(x=100, y=200, width=100, height=100)
 
-    bouton_b_l = ttk.Button(fenetre, text="7", command=button_b_l_clicked)
-    bouton_b_l.place(x=200, y=0, width=100, height=100)
-    bouton_b_c = ttk.Button(fenetre, text="8", command=button_b_c_clicked)
-    bouton_b_c.place(x=200, y=100, width=100, height=100)
-    bouton_b_r = ttk.Button(fenetre, text="9", command=button_b_r_clicked)
-    bouton_b_r.place(x=200, y=200, width=100, height=100)
+        bouton_b_l = ttk.Button(self.fenetre, text="7", command=self.button_b_l_clicked)
+        bouton_b_l.place(x=200, y=0, width=100, height=100)
+        bouton_b_c = ttk.Button(self.fenetre, text="8", command=self.button_b_c_clicked)
+        bouton_b_c.place(x=200, y=100, width=100, height=100)
+        bouton_b_r = ttk.Button(self.fenetre, text="9", command=self.button_b_r_clicked)
+        bouton_b_r.place(x=200, y=200, width=100, height=100)
 
-    fenetre.mainloop()
-
+        self.fenetre.mainloop()
+"""
 def nom_joueurs_lb():
     est_valide = False
     while est_valide == False:  #Pour toujours demander si l'input retourne une erreur
@@ -104,10 +88,15 @@ print("Bienvenue au Morpion!")
 #Variable Globale
 resultat = "Aucun"
 joueurs = nom_joueurs_lb()
+"""
+joueurs = [["test", "O"],["testttt", "X"]]
+"""
 if joueurs[1][0] == "Bot":
     difficulte = input("Choissisez la difficulté: (F)acile, [defaut](N)ormal, (W)arGames. -> ")
+"""
 joueur_actuel = 1   # Détermine le joueur en cours (1er joueur = 0)
 grille = [["","",""],["","",""],["","",""]]
+"""
 tour = 0
 
 #Boucle de tour
@@ -135,3 +124,8 @@ elif resultat == "Egalité!":
     print(f"\nC'est une égalité!")                                  #label
 else:
     print("Erreur!")                                                #label
+"""
+
+
+
+affichage_grille_tk(grille, joueurs, joueur_actuel)
